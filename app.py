1d0eb886e60fd18478dbb1dbdf860b5c4f19e305
Dmitriy M <dmak1984@gmail.com>
2023-04-24 17:29:12 +0300

    trying to render interactive table, no success ((

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from shiny import *
+from shinywidgets import output_widget, render_widget
+import ipywidgets as widgets
 import numpy as np
 import matplotlib.pyplot as plt
+
 from SDRP import SDRP
 from subr_fit import fit_params, fit_uncertainties
 from constants import *
@@ -9,9 +12,11 @@ app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!"),
     ui.input_file("input_files", "Load your spectra", accept=["*.*"], multiple=True),
     ui.input_action_button("b_preview", "Preview"),
+    output_widget("spectra_info"),
     ui.output_plot("preview_data"),
 )
 
+#
 
 def server(input, output, session):
     @output
@@ -25,11 +30,37 @@ def server(input, output, session):
         f: list[FileInfo] = input.input_files()
         fig, ax = plt.subplots(1, len(f), sharey=True)
         print('length of files dataset is ', len(f), ' files')
+        # f has fields: name, size, type, datapath
+        # global fnames
+        # fnames = list()
+        # for item in f:
+        #     fnames.append(item["name"])
         for ifil in range(len(f)):
             cur_data = np.loadtxt(f[ifil]["datapath"], comments='//')
+            if cur_data[0, 0] < 1000:
+                cur_data[:, 0] = cur_data[:, 0] * 1000.
             ax[ifil].plot(cur_data[:,0]*0.001, cur_data[:,1]/np.max(cur_data[:,1]), 'ro')
             #ax = plt.subplot(ifil, cur_data[:,0]*0.001, cur_data[:,1]/np.max(cur_data[:,1]))
+        #print(fnames)
+        spectra_info()
         return fig
+
+    # @render_widget
+    # def spectra_info():
+    #     if input.input_files is None:
+    #         return
+    #     f: list[FileInfo] = input.input_files()
+    #     fnames = list()
+    #     for item in f:
+    #         fnames.append(item["name"])
+    #     for fname in fnames:
+    #         items = [widgets.Label(fname),
+    #                  widgets.FloatText(value=0., description='P self, Torr'),
+    #                  widgets.FloatText(value=0., description='P foreign, Torr'),
+    #                  widgets.FloatText(value=0., description='T, K'),
+    #                  widgets.Dropdown(options=["VID", "RAD", "CAV"], description="Type")]
+    #         yield widgets.HBox(items)
+
 
 
 app = App(app_ui, server)
